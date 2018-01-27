d86703bec6d5919c0ab8792caa321059ba9c6dbb
chaitype <chai41104@hotmail.com>
2018-01-27 21:45:16 +0000

    Adding modify page

---
+++ src/Modify.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+import './App.css';
+
+class Modify extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      	ASDASD
+      </div>
+    );
+  }
+}
+
+export default Modify;
