0197bbdad832d776c07c0b4fe7e1972cb20da270
chaitype <chai41104@hotmail.com>
2018-01-27 22:01:54 +0000

    Adding title in Modify

--- src/Modify.js
+++ src/Modify.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import AppBar from '../node_modules/material-ui/AppBar';
 
 import './App.css';
 
@@ -7,10 +7,35 @@ class Modify extends Component {
   constructor(props) {
     super(props);
   }
+  displayTitle(isWarning) {
+  	if (isWarning) {
+  		return (
+  			<AppBar
+	          title="Warning Some parameters are not possible. Please adjust setting."
+	          iconElementLeft={
+	          	<i class="material-icons" style={{fontSize: 45}}>error</i>
+	          }
+	          iconElementRight={<div/>}
+	          style={{backgroundColor: '#d50000'}}
+	        />
+  		);
+  	} else {
+	  	return (
+	  		<AppBar
+	          title="Warning Some parameters are not possible. Please adjust setting."
+	          iconElementLeft={
+	          	<i class="material-icons" style={{fontSize: 45}}>error</i>
+	          }
+	          iconElementRight={<div/>}
+	          style={{backgroundColor: '#d50000'}}
+	        />
+	  	);
+  	}
+  }
   render() {
     return (
       <div>
-      	ASDASD
+      	{this.displayTitle(true)}
       </div>
     );
   }
