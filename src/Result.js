e6ff7a63961c16a773c37830f29b8a69403f5c09
Alessandro Caianiello <alessandro@caianiello.it>
2018-01-28 12:54:51 +0000

    Styles

--- src/Result.js
+++ src/Result.js
@@ -72,19 +72,19 @@ class Result extends Component {
             icon={<i className="material-icons">print</i>}
             style={{width: '30%'}}
             label="Export to PDF"
-            backgroundColor={'#5cb85c'}
+            backgroundColor={'#f1c40f'}
           />
           <FlatButton
             icon={<i className="material-icons">screen_share</i>}
             style={{width: '30%'}}
             label="Share to Result"
-            backgroundColor={'#5cb85c'}
+            backgroundColor={'#f1c40f'}
           />
           <FlatButton
             icon={<i className="fa fa-facebook"></i>}
             style={{width: '30%'}}
             label="Post on Facebook"
-            backgroundColor={'#5cb85c'}
+            backgroundColor={'#f1c40f'}
           />
         </CardActions>
       </Card>
